5a9034cc5dcd4c09eeefa69beff844eafb5ce96e
hujinqiang <756677154@qq.com>
2021-06-23 14:29:33 +0800

    初始化

---
+++ moon-user/moon-user/src/main/java/com/moon/mall/user/UserApplication.java
@@ -0,0 +1,17 @@
+package com.moon.mall.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * C端用户服务
+ *
+ */
+@SpringBootApplication
+public class UserApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
